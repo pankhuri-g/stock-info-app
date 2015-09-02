5420df28d71a1a8814cdd7a579c55fc3a5501910
pankhuri-g <pankhuri.taneja@gmail.com>
2015-09-02 19:28:09 -0400

    graph page added

--- app.py
+++ app.py
@@ -13,10 +13,8 @@ def index():
   if request.method == 'GET':
      return render_template('index.html')
   else:
-     #request was post
      app.symbol=request.form['symbol']
-     #message = 
-     return "Symbol was %s", app.symbol
+     return render_template('graph.html',ticker=app.symbol)
 
 if __name__ == '__main__':
   app.run(port=33507)

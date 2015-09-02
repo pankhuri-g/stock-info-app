5cdace04b930b7985a43099b065ad7027f43d35e
pankhuri-g <pankhuri.taneja@gmail.com>
2015-09-02 19:03:17 -0400

    get and post testing

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+app.symbol = "NONE"
+
 
 @app.route('/')
 def main():
   return redirect('/index')
 
-@app.route('/index')
+@app.route('/index', methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+  if request.method == 'GET':
+     return render_template('index.html')
+  else:
+     #request was post
+     app.symbol=request.form['symbol']
+     #message = 
+     return "Symbol was %s", app.symbol
 
 if __name__ == '__main__':
   app.run(port=33507)

d4cf1ca1aec0e3cea7c89c253693584097c00da2
pankhuri-g <pankhuri.taneja@gmail.com>
2015-09-03 21:21:54 -0400

    plot refined

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from bokeh.resources import CDN
 from bokeh.embed import file_html
 from bokeh.util.string import encode_utf8
 import requests                                                                                                                    
-#import pandas                                                                                                                     
 import simplejson                                                                                                                  
 from datetime import datetime                                                                                                      
                                                 
@@ -12,7 +11,7 @@ app = Flask(__name__)
 app.symbol = "NONE" 
 
 def build_graph(ticker):                                                                                   
-  url = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ticker.upper()+'.json?column_index=4&start_date=2015-08-01&end_date=2015-08-31&order=asc' 
+  url = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ ticker.upper() +'.json?column_index=4&start_date=2015-08-01&end_date=2015-08-31&order=asc' 
   data = requests.get(url).text                                                                                                    
   data = simplejson.loads(data)
 
@@ -23,8 +22,11 @@ def build_graph(ticker):
 
   #use lists generated as x and y axes
   plot = figure(x_axis_type = "datetime")
-  plot.line(stock_date, stock_value)
-  plot.title = "Stock Closing Prices-"+ticker.upper()
+  plot.line(stock_date, stock_value, legend=ticker.upper()+': Close')
+  plot.title = ticker.upper() + ": Aug-2015 Data from Quandl WIKI"
+  plot.xaxis.axis_label = 'Date'
+  plot.yaxis.axis_label = 'Price'
+  plot.circle(stock_date, stock_value)
   html = file_html(plot, CDN, ticker.upper()+" Closing Price plot")
   return html
 
@@ -40,7 +42,6 @@ def index():
 @app.route('/graph',methods=['POST'])
 def graph():
     app.symbol=request.form['symbol']       
-    #return render_template('graph.html',ticker=app.symbol, mydata=5)#appLogic.build_graph(app.symbol))
     graph_html = build_graph(str(app.symbol))
     return encode_utf8(graph_html)   
  

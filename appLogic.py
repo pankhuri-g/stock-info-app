540f5037105e97c3ca58ef5d75cc636765011563
pankhuri-g <pankhuri.taneja@gmail.com>
2015-09-07 04:26:50 -0400

    removed two files

--- appLogic.py
+++
@@ -1,7 +0,0 @@
-import Quandl
-import pandas
-
-def build_graph(ticker):
-
-  mydata = Quandl.get("WIKI/"+ticker.upper()+".4", trim_start="2015-08-01", trim_end="2015-08-31")
-  return float(mydata.ix[2,'Close'])
